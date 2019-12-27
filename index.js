dbe752e4b1e4ad0d7691c4379fd2a78358fd2176
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2019-12-27 15:45:51 -0300

    Inserted comments.

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const path = require('path');
 
 const app = express();
 
-//conexão com o sqlite
+/**
+ * Conexão com o sqlite
+ */
+
 const sqlite = require('sqlite');
 const dbConection = sqlite.open(path.resolve(__dirname, 'banco.sqlite'), { Promise });
 
@@ -13,7 +16,11 @@ const port = process.env.PORT || 3000;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/**
+ * Home
+ */
 
 app.get('/', async (request, response) => {
   const db = await dbConection;
@@ -30,6 +37,10 @@ app.get('/', async (request, response) => {
   });
 });
 
+/**
+ * Vaga
+ */
+
 app.get('/vaga/:id', async (request, response) => {
   const db = await dbConection;
   const vaga = await db.get('select * from vagas where id = ' + request.params.id);
@@ -37,6 +48,10 @@ app.get('/vaga/:id', async (request, response) => {
     vaga
   });
 });
+
+/**
+ * Admin vagas
+ */
 
 app.get('/admin', (req, res) => {
   res.render('admin/home');
@@ -76,23 +91,23 @@ app.get('/admin/vagas/editar/:id', async (req, res) => {
 
 app.post('/admin/vagas/editar/:id', async (req, res) => {
   const { titulo, descricao, categoria } = req.body;
-  //const { id } = req.params;
   const db = await dbConection;
   await db.run(`update vagas set categoria = ${categoria}, titulo = '${titulo}', descricao = '${descricao}' where id = ` + req.params.id);
   res.redirect('/admin/vagas');
 });
 
-
+/**
+ * Cria as tabelas caso não existam
+ */
 
 const init = async () => {
   const db = await dbConection
   await db.run('create table if not exists categorias (id INTEGER PRIMARY KEY, categoria TEXT);');
   await db.run('create table if not exists vagas (id INTEGER PRIMARY KEY, categoria INTEGER, titulo TEXT, descricao TEXT);');
-  //const categoria = 'Marketing team';
-  //await db.run(`insert into categorias(categoria) values ('${categoria}');`);
 }
 
 init();
+
 app.listen(port, (err) => {
   if (err) {
     console.log('Não foi possivel iniciar o servidor do Jobify');
