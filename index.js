8a19abf0756f7957cadd4a3fea589fcd836f7f8a
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2019-12-27 00:02:01 -0300

    Resolve path.

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const dbConection = sqlite.open(path.resolve(__dirname, 'banco.sqlite'), { Promi
 
 const port = process.env.PORT || 3000;
 
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.resolve(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', async (request, response) => {
